631df6904e1c08f6683c473cce20d4bbd8740984
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-04 22:06:03 +0300

    Initialize Firebase app and authentication

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWbXT_sP3AE_YnYDiUf338MLPXpf2kk2s",
+  authDomain: "mecd-voice-7beea.firebaseapp.com",
+  projectId: "mecd-voice-7beea",
+  storageBucket: "mecd-voice-7beea.firebasestorage.app",
+  messagingSenderId: "203377764807",
+  appId: "1:203377764807:web:63f65a4c24fc944a2951e2"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
