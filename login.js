80e4d4a276de86fbbb25ff79bf6dc2ffed65d68e
mecdjhjhnh37-collab <mecdjhjhnh37@gmail.com>
2026-08-05 12:00:56 +0300

    Update login.js

--- login.js
+++ login.js
@@ -1,9 +1,15 @@
-import { auth } from "./firebase.js";
+import { auth, db } from "./firebase.js";
 
 import {
   GoogleAuthProvider,
   signInWithPopup
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  doc,
+  getDoc,
+  setDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const lang = localStorage.getItem("language") || "ar";
 
@@ -31,12 +37,29 @@ back.onclick = () => {
 google.onclick = async () => {
   try {
     const provider = new GoogleAuthProvider();
-
     const result = await signInWithPopup(auth, provider);
 
-    localStorage.setItem("userName", result.user.displayName || "");
-    localStorage.setItem("userPhoto", result.user.photoURL || "");
-    localStorage.setItem("userEmail", result.user.email || "");
+    const user = result.user;
+
+    const userRef = doc(db, "users", user.uid);
+    const userSnap = await getDoc(userRef);
+
+    if (!userSnap.exists()) {
+      await setDoc(userRef, {
+        uid: user.uid,
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL,
+        admin: user.email === "mecdjhjh37@gmail.com",
+        online: true,
+        createdAt: Date.now()
+      });
+    }
+
+    localStorage.setItem("userName", user.displayName || "");
+    localStorage.setItem("userPhoto", user.photoURL || "");
+    localStorage.setItem("userEmail", user.email || "");
+    localStorage.setItem("userId", user.uid);
 
     window.location.href = "home.html";
 
